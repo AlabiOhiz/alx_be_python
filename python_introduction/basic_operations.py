37abcc2629cc5628baf69a02723c094d4a7b1ef9
Daniel Alabi <alabiohidaniel@gmail.com>
2025-11-16 21:43:58 +0100

    New file added

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,19 @@
+#Define variables
+number1 = 10
+number2 = 5
+
+#--- calculation----
+
+# calulate the sum
+sum_result = number1 + number2
+
+# calculate the difference
+difference_result = number1 - number2
+
+# caiculate the product 
+product_result = number1 * number2
+
+# Print Results
+print(f"The sum of {number1} and {number2} is {sum_result}")
+print(f"The difference of {number1} and {number2} is {difference_result}")
+print(f"The product of {number1} and {number2} is {product_result}")
---
+++ python_introduction/rectangle_area.py
@@ -0,0 +1,11 @@
+# Define variables
+length = 10    # Length of the rectangle
+width = 5   # Width of the rectangle
+
+# --- Calculation ---
+
+# Calutate the Area of a rectangle
+area = length * width
+
+# Print area of a Rectangle
+print(f"The area of the: {area}")
