57b107fd0d3cdb203414d266839c29c322f8b3c8
Daniel Alabi <alabiohidaniel@gmail.com>
2025-11-18 13:01:04 +0100

    remove the varible seconds_per_hours

--- python_introduction/hours_to_seconds.py
+++ python_introduction/hours_to_seconds.py
@@ -4,7 +4,7 @@ hours = 2
 
 # Calculate hours to seconds
 
-seconds_per_hours = 3600
-seconds = hours * seconds_per_hours
+seconds = 3600 * hours
+
 # Print hours to seconds
 print(f"{hours} hour(s) is {seconds} seconds")
