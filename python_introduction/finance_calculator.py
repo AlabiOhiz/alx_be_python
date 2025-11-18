a9d70b312b4c950fd845ffd87be7f7103006108e
Daniel Alabi <alabiohidaniel@gmail.com>
2025-11-18 11:18:03 +0100

    Change made (int to float) on the finance calculator

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -5,21 +5,21 @@ year_to_add = 27
 # Prompt the user for their monthly income
 
 monthly_income_input = input ("Enter your monthly income:")
-monthly_income = int(monthly_income_input)
+monthly_income = float(monthly_income_input)
 
 # Prompt the user for their total monthly expenses
 
 total_monthly_expenses_input = input ("Enter your total monthly expenses:")
-total_monthly_expenses = int(total_monthly_expenses_input)
+monthly_expenses = float(total_monthly_expenses_input)
 
 # Calculate Monthly Savings
 
-monthly_savings = monthly_income - total_monthly_expenses
+monthly_savings = monthly_income - monthly_expenses
 
 # Calculate Project Annual Savings
 
 projected_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
 
 # Print the final result
-print(f"Your monthly saving are {monthly_income}")
+print(f"Your monthly saving are {monthly_savings}")
 print(f"Projected savings after one year, with interest, is: {projected_savings}")
