b8227a7d7a59fd89217cf58832c35721ee4feaf0
Daniel Alabi <alabiohidaniel@gmail.com>
2025-11-30 15:52:00 +0100

    changed list to range

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,7 +1,7 @@
 
-times = [1,2,3,4,5,6,7,8,9,10]
+
 num = int(input("Enter a number to see its multiplication table: "))
 
-for time in times:
+for time in range(1,11):
     multi = time * num
     print(f"{num} * {time} = {multi} ")
