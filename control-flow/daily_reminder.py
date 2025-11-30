693e01579d76b1a07684b008a19ca53af12f75ac
Daniel Alabi <alabiohidaniel@gmail.com>
2025-11-30 17:10:53 +0100

    daily reminder file added to dir

---
+++ control-flow/daily_reminder.py
@@ -0,0 +1,31 @@
+
+task = input("Enter your task: ").lower()
+
+priority = input("Priority (high/medium/low):  ").lower()
+time_bound = input("Is it time-bound? (yes/no):  ").lower()
+
+
+match priority:
+    case "high":
+        if time_bound == "yes":
+            print(f" '{task}' is a high priority task that requires immediate attention today!")
+        elif time_bound == "no":
+            print(f"'{task}' is a high priority task that requires to create time get it done before it urgent!")
+        else:
+            print("Invaild Response")
+    case "medium":
+        if time_bound == "yes":
+            print(f"'{task}' is a medium priority task. Add a plan to get it done before it too late")
+        elif time_bound == "no":
+            print(f"'{task}' is a medium priority task. please plan a time to get this done")
+        else:
+            print("Invaild Response")
+    case "low":
+        if time_bound == "yes":
+            print(f"'{task}' is a low priority task. Consider completing it soon just on time")
+        elif time_bound == "no":
+            print(f" {task}  is a low priority task. Consider completing it when you have free time.")
+        else:
+            print("Invaild Response")
+    case _:
+        print("Invalid priority entered.")
